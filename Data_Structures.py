f2461062aef71d78fd037e22177b4512f5e23409
Brandon <bredmond1019@gmail.com>
2021-01-07 12:00:02 -0500

    Worked on Queue & Deque

--- Data_Structures.py
+++ Data_Structures.py
@@ -1,14 +1,25 @@
+"""
+Stack
+
+Stack is First In, Last Out
+
+"""
+
+
 class Stack:
 
     def __init__(self):
         self.stack = []
+        self.len_stack = 0
 
     def push(self, e):
         self.stack.append(e)
+        self.len_stack += 1
 
     def pop(self):
         if not self.empty():
-            self.stack.pop(len(self.stack) - 1)
+            self.stack.pop(self.len_stack - 1)
+            self.len_stack -= 1
 
     def top(self):
         if not self.empty():
@@ -16,9 +27,49 @@ class Stack:
         return None
 
     def empty(self):
-        if len(self.stack) == 0:
+        if self.len_stack == 0:
             return True
         return False
 
     def length(self):
-        return len(self.stack)
+        return self.len_stack
+
+
+"""
+Queue
+
+Queue is First in, First Out
+
+"""
+
+
+class Queue:
+
+    def __init__(self):
+        self.queue = []
+        self.len_queue = 0
+
+    def push(self, e):
+        self.queue.append(e)
+        self.len_queue += 1
+
+    def pop(self):
+        if not self.empty():
+            self.queue.pop(0)
+
+    def empty(self):
+        if self.len_queue == 0:
+            return True
+        return False
+
+    def length(self):
+        return self.len_queue
+
+    def front(self):
+        if not self.empty():
+            return self.queue[0]
+        return "The Queue is Empty"
+
+
+q = Queue()
+print(q.front())
